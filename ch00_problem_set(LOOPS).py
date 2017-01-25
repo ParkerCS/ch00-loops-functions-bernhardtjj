07226a34b423ca4e1bf94868a5beaa9e2618332f
bernhardtjj <jbernhardt@fwparker.org>
2017-01-25 10:03:40 -0600

    Do the loops start

--- ch00_problem_set(LOOPS).py
+++ ch00_problem_set(LOOPS).py
@@ -1,12 +1,20 @@
 # LOOPS (22pts TOTAL)
 
 # PROBLEM 1 (Fibonacci - 4pts)
-## The Fibonacci sequence is a sequence of numbers that starts with 1, followed by 1 again.
+# The Fibonacci sequence is a sequence of numbers that starts with 1, followed by 1 again.
 # Every next number is the sum of the two previous numbers.
 # I.e., the sequence starts with 1, 1, 2, 3, 5, 8, 13, 21,...
 # Write a program that calculates and prints the Fibonacci sequence
 # until the numbers get higher than 1000.
-
+yesterday = 0
+today = 1
+tomorrow = 1
+print(1, end=' ')
+while today < 1000:
+    tomorrow = yesterday + today
+    yesterday = today
+    today = tomorrow
+    print(today, end=' ')
 
 # PROBLEM 2 (Number Guessing Game - 6pts)
 # Write a program that takes a random integer between 1 and 1000
@@ -16,7 +24,22 @@
 # and “You guessed it!” if the number it took is equal to the number
 # It might be wise, for testing purposes, to also display the number that the
 # program randomly picks, until you are sure that the program works correctly
-
+import random
+print()
+number = random.randrange(1, 1001)
+guess = 0
+while guess != number:
+    try:
+        guess = int(input("Give me a guess: "))
+        if 1000 < guess or guess < 1:
+            raise ValueError
+        if guess < number:
+            print("Too low.")
+        elif guess > number:
+            print("Too high.")
+    except ValueError:
+        print("Has to be an integer between 1 and 1000")
+print("You are right.")
 # PROBLEM 3 (Dice Hi-Low - 6pts)
 # You roll five six-sided dice, one by one.
 # How big is the probability that the value of each die
@@ -31,4 +54,3 @@
 # What are the digits?
 # Note: to make ABCD and DCBA conventional numbers, neither A nor D can be zero.
 # Use a quadruple-nested loop to solve.
-
