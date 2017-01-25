bb1f06f4ed098039a0f9761d58213706d597a28b
bernhardtjj <jbernhardt@fwparker.org>
2017-01-25 15:32:12 -0600

    Finished the loops

--- ch00_problem_set(LOOPS).py
+++ ch00_problem_set(LOOPS).py
@@ -1,4 +1,5 @@
 # LOOPS (22pts TOTAL)
+import random
 
 # PROBLEM 1 (Fibonacci - 4pts)
 # The Fibonacci sequence is a sequence of numbers that starts with 1, followed by 1 again.
@@ -24,7 +25,8 @@ while today < 1000:
 # and “You guessed it!” if the number it took is equal to the number
 # It might be wise, for testing purposes, to also display the number that the
 # program randomly picks, until you are sure that the program works correctly
-import random
+
+
 print()
 number = random.randrange(1, 1001)
 guess = 0
@@ -40,6 +42,7 @@ while guess != number:
     except ValueError:
         print("Has to be an integer between 1 and 1000")
 print("You are right.")
+
 # PROBLEM 3 (Dice Hi-Low - 6pts)
 # You roll five six-sided dice, one by one.
 # How big is the probability that the value of each die
@@ -48,9 +51,32 @@ print("You are right.")
 # but “1, 1, 4, 3, 6” is not. Determine the
 # probability of success using a simulation of a large number of trials.
 
+success = 0
+failure = 0
+print()
+print("Calculating using one million trials:")
+for i in range(1000000):
+    die_list = []
+    for die in range(6):
+        die_list.append(random.randrange(1, 7))
+    if (die_list[0] <= die_list[1]) and (die_list[1] <= die_list[2]) and (die_list[2] <= die_list[3]) and (
+                die_list[3] <= die_list[4]) and (die_list[4] <= die_list[5]):
+        success += 1
+    else:
+        failure += 1
+print(round(success / (success + failure) * 100, 2), "%")
+
 # PROBLEM 4 (Number Puzzler - 6pts)
 # A, B, C, and D are all different digits.
 # The number DCBA is equal to 4 times the number ABCD.
 # What are the digits?
 # Note: to make ABCD and DCBA conventional numbers, neither A nor D can be zero.
 # Use a quadruple-nested loop to solve.
+print()
+for A in range(1, 10):
+    for B in range(10):
+        for C in range(10):
+            for D in range(1, 10):
+                if int(str(D) + str(C) + str(B) + str(A)) == 4 * int(str(A) + str(B) + str(C) + str(D)):
+                    print(A, B, C, D);
+                    quit()
